6f6ac774a1cae62f6ab6cf288356a0593cde062e
chokchaiperf <chokchai@perfenterprise.com>
2026-06-11 12:13:44 +0700

    Fix formatting of setup instruction message in Telegram notification script

--- scripts/notify-telegram.sh
+++ scripts/notify-telegram.sh
@@ -259,7 +259,7 @@ _append_download_links() {
         MESSAGE+="${NL}🔥 <b>Link Download</b>  <a href=\"${FIREBASE_URL}\">Firebase app distribute</a>${NL}"
       fi
       if [ -n "${FIREBASE_SETUP_URLS:-}" ]; then
-        MESSAGE+="setup instruction <b>  </b>${NL}"
+        MESSAGE+="setup instruction <b>  </b>"
         local item label url
         for item in ${FIREBASE_SETUP_URLS}; do
           label="${item%%=*}"
